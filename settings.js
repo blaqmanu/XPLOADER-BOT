0c870bf31ad5afd7d2dfb5a5b25e4c45d3488f18
Blaq Hax <barakabarak54@gmail.com>
2024-11-27 22:39:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ const { color } = require('./lib/color')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 //=================================================//
 
-global.SESSION_ID = process.env.SESSION_ID || ''
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER~K7AHxTxK#RqjfQ8NodXYVGkkBHoYPerz4gtzhznwvvCspDlPkmn4XPLOADER~K7AHxTxK#RqjfQ8NodXYVGkkBHoYPerz4gtzhznwvvCspDlPkmn4'
 //Enter your Xploader session id here; must start with XPLOADER~
 
 //=================================================//
@@ -25,7 +25,7 @@ global.botname = process.env.BOT_NAME || '𝐗𝐩𝐥𝐨𝐚𝐝𝐞𝐫𝐁�
 
 //=================================================//
 
-global.ownernumber = process.env.OWNER_NUMBER || '254796180105'
+global.ownernumber = process.env.OWNER_NUMBER || '254751551511'
 //Type your number here
 
 //=================================================//
